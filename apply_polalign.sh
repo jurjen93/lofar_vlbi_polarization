213dcb9dd40996b9d981c8b56f365e26a8df6dc9
jurjen93 <jurjendejong93@gmail.com>
2025-03-21 09:00:02 +0100

    t

--- apply_polalign.sh
+++ apply_polalign.sh
@@ -2,7 +2,7 @@
 
 #INPUT
 REGION=$1
-H5_IN=$2
+MS_IN=$2
 REF_RM=$3
 REF_OFFSET=$4
 
@@ -14,6 +14,6 @@ python $SCRIPT_DIR/scripts/polalign.py \
   --input_directory "./" \
   --output_directory "./" \
   --region_file ${REGION} \
-  --input_h5 ${H5_IN} \
+  --input_ms ${MS_IN} \
   --ref_RM ${REF_RM} \
   --ref_offset ${REF_OFFSET}
