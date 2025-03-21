b333b54420267a9995ec72485ea63a89fb5258d9
jurjen93 <jurjendejong93@gmail.com>
2025-03-21 09:01:24 +0100

    t

--- scripts/utils/fits_handling.py
+++ scripts/utils/fits_handling.py
@@ -36,38 +36,33 @@ def make_freq_vec(Ifits_files):
     return freqvec
 
 
-import numpy as np
-from astropy.io import fits
-
-
 def make_image_cube(Ifits_files, return_noise=False):
     """
     Make image cube
     Args:
-        Ifits_files: List of Stokes I FITS files
-        return_noise: Whether to return noise array
+        Ifits_files: Stokes I fits files
+        noise: Return noise array
 
     Returns: Cube, noise_array (optional)
     """
 
     with fits.open(Ifits_files[0]) as hdul:
         template = np.squeeze(hdul[0].data)
-        if template.size == 0:
-            raise ValueError(f"Error: First FITS file {Ifits_files[0]} contains no data.")
 
-    cube = np.zeros((len(Ifits_files), *template.shape))
-    noise_array = np.zeros(len(Ifits_files))
+    cube = np.zeros((len(Ifits_files), template.shape[0], template.shape[0]))  # freq axis first (RMtools wants this)
+    noise_array = np.zeros((len(Ifits_files)))
+    print(cube.shape, noise_array.shape)
 
     for image_idx, image in enumerate(Ifits_files):
+        print(image)
         with fits.open(image) as hdul:
-            data = np.squeeze(hdul[0].data)
-            if data.size == 0:
-                raise ValueError(f"Error: FITS file {image} contains no data.")
-            cube[image_idx, :, :] = data
+            cube[image_idx, :, :] = np.squeeze(hdul[0].data)
             if return_noise:
-                noise_array[image_idx] = findrms(data)
-
-    return (cube, noise_array) if return_noise else (cube, None)
+                noise_array[image_idx] = findrms(np.squeeze(hdul[0].data))
+    if return_noise:
+        return cube, noise_array
+    else:
+        return cube, None
 
 
 def flatten(f):
