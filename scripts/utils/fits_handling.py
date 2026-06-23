cf8bf98dab618a403930d0d2c5f58961f4a4fe24
jurjen93 <jurjendejong93@gmail.com>
2026-06-23 14:33:09 +0200

    circ import

--- scripts/utils/fits_handling.py
+++ scripts/utils/fits_handling.py
@@ -2,7 +2,26 @@ from astropy.io import fits
 from astropy.wcs import WCS
 import numpy as np
 
-from .image_handling import findrms
+
+def findrms(mIn, maskSup=1e-7):
+    """
+    find the rms of an array, from Cycil Tasse/kMS
+    """
+    try:
+        m = mIn[np.abs(mIn) > maskSup]
+        rmsold = np.std(m)
+        diff = 1e-1
+        cut = 3.
+        bins = np.arange(np.min(m), np.max(m), (np.max(m) - np.min(m)) / 30.)
+        med = np.median(m)
+        for i in range(10):
+            ind = np.where(np.abs(m - med) < rmsold * cut)[0]
+            rms = np.std(m[ind])
+            if np.abs((rms - rmsold) / rmsold) < diff: break
+            rmsold = rms
+        return rms
+    except ValueError:
+        return np.nan
 
 
 def get_header(fits_file):
--- scripts/utils/image_handling.py
+++ scripts/utils/image_handling.py
@@ -6,27 +6,6 @@ from .fits_handling import flatten, make_freq_vec, make_noise_vec
 
 
 
-def findrms(mIn, maskSup=1e-7):
-    """
-    find the rms of an array, from Cycil Tasse/kMS
-    """
-    try:
-        m = mIn[np.abs(mIn) > maskSup]
-        rmsold = np.std(m)
-        diff = 1e-1
-        cut = 3.
-        bins = np.arange(np.min(m), np.max(m), (np.max(m) - np.min(m)) / 30.)
-        med = np.median(m)
-        for i in range(10):
-            ind = np.where(np.abs(m - med) < rmsold * cut)[0]
-            rms = np.std(m[ind])
-            if np.abs((rms - rmsold) / rmsold) < diff: break
-            rmsold = rms
-        return rms
-    except ValueError:
-        return np.nan
-
-
 def clipped_median(data, sigma_clip=3.0, max_iter=5):
     """
     Compute the sigma-clipped median of a one-dimensional array.
