0fa2a89c8870b0bda076762c57e3361a6078d310
jurjen93 <jurjendejong93@gmail.com>
2026-06-25 15:30:07 +0200

    plot fix

--- scripts/polalign.py
+++ scripts/polalign.py
@@ -203,9 +203,9 @@ def fit_RM(i_fits: list = None, u_fits: list = None, q_fits: list = None, region
         / (Uflux[sort_lam] ** 2 + Qflux[sort_lam] ** 2) ** 2
     )
 
-    lam2_s = np.linspace(lambda2.min(), lambda2.max(), 500)
+    lam2_s = np.linspace(lambda2.min(), lambda2.max(), 5000)
     polangle_model = fitQU_depol[1]*lam2_s + fitQU_depol[2]
-    polangle_model = (polangle_model + np.pi/2) % np.pi - np.pi/2
+    polangle_model = polangle_model % np.pi - np.pi/2
 
     fig, ax = plt.subplots(figsize=(12, 7))
     ax.errorbar(lambda2[polangle_sigma<1], polangle[polangle_sigma<1], yerr=polangle_sigma[polangle_sigma<1],
